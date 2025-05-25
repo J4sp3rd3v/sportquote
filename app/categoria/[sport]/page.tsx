056a2d712d476b4bc5a2a23d94a638a40d4cc6b5
SitoSport Developer <sitosport@example.com>
2025-05-25 19:20:50 +0100

    Force rebuild: Aggiunto commento per forzare nuovo build Vercel

--- app/categoria/[sport]/page.tsx
+++ app/categoria/[sport]/page.tsx
@@ -247,7 +247,7 @@ export default function CategoryPage() {
 
       {/* Main Content */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        {/* Data Source Toggle */}
+        {/* Data Source Toggle - Fixed TypeScript props */}
         <DataSourceToggle
           useRealData={useRealData}
           onToggle={toggleDataSource}
